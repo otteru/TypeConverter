2811478830fc6db9b4ba6d0086608f77f7a09738
otteru <otteru@naver.com>
2024-10-10 11:01:16 +0900

    스프링에 Converter 적용하기

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    //hello
 
     @GetMapping("/hello-v1")
     public String helloV1(HttpServletRequest request) {
